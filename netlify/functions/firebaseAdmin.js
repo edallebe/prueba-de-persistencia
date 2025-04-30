4f73cfd63f235f55c98d195e76b1832e07d87c2b
Alexis Leiva <alexisleivabello072020@gmail.com>
2025-04-30 11:36:36 -0500

    configuracion

--- netlify/functions/firebaseAdmin.js
+++ netlify/functions/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
- 
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
@@ -9,5 +9,5 @@ if (!admin.apps.length) {
     }),
   });
 }
- 
+
 module.exports = admin;
