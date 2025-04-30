4ea4b575538ad0390677e4e2b116f97dc6aedbea
Alexis Leiva <alexisleivabello072020@gmail.com>
2025-04-30 11:37:07 -0500

    nueva

--- netlify/functions/getUser.js
+++ netlify/functions/getUser.js
@@ -1,22 +1,31 @@
-const admin = require('./firebaseAdmin.js');
- 
+const admin = require('./firebaseAdmin');
+
 exports.handler = async (event, context) => {
   try {
     const userId = event.queryStringParameters.id;
+
+    if (!userId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'ID de usuario requerido' }),
+      };
+    }
+
     const userDoc = await admin.firestore().collection('users').doc(userId).get();
- 
+
     if (!userDoc.exists) {
       return {
         statusCode: 404,
         body: JSON.stringify({ error: 'Usuario no encontrado' }),
       };
     }
- 
+
     return {
       statusCode: 200,
       body: JSON.stringify(userDoc.data()),
     };
   } catch (error) {
+    console.error(error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Error al obtener el usuario' }),
